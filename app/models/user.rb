88a4301266ec7113fffc1c10b97e096134d50868
yuuuuyas <yuyasakurako0110@gmail.com>
2021-12-22 16:17:22 +0900

    アソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,5 +21,5 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角の英字と数字の両方を含めて設定してください' 
 
-
+  has_many :items
 end
