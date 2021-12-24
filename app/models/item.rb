e1365b598db6cba9abad78e19d9d6a272f59e338
yuuuuyas <yuyasakurako0110@gmail.com>
2021-12-24 15:13:41 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,13 @@ class Item < ApplicationRecord
     belongs_to :delivery_date
 
     validates :category_id, :status_id, :delivery_caharge_id, :area_id, :delivery_date, numericality: { other_than: 1 }
+    validates :image, presence: true
+    validates :item_name, presence: true
+    validates :explanation, presence: true
+    validates :category_id, presence: true
+    validates :status_id, presence: true
+    validates :delivery_caharge_id, presence: true
+    validates :area_id, presence: true
+    validates :price, presence: true
+    
 end
