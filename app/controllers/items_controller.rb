b6f25fc630eba5ab35ca01c6f7e4ab629837ccde
yuuuuyas <yuyasakurako0110@gmail.com>
2022-01-05 16:40:10 +0900

    コントローラーの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,9 +21,18 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  #def edit
-    #@item = Item.find(params[:id])
-  #end
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(items_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
 
   private
   def items_params
