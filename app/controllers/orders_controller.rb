d506bb7b68cb9a376ec9a7591c6ca12f4c06faa2
yuuuuyas <yuyasakurako0110@gmail.com>
2022-01-13 11:12:40 +0900

    条件分岐の追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,19 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
     @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id || @item.order.present?
+      redirect_to root_path
+    end  
     @order_address = OrderAddress.new 
   end
 
   def create
     @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id || @item.order.present?
+      redirect_to root_path
+    end
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       pay_item
