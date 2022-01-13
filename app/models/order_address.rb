1cbb9fec89832d472cd0fbc37a58a3b4df16a788
yuuuuyas <yuyasakurako0110@gmail.com>
2022-01-13 16:48:10 +0900

    バリデーション修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -9,7 +9,7 @@ class OrderAddress
     validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :house_number
-    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid' }
     validates :token
   end
 
