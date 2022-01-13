4ea56d2a78c0237bbe010f04c728318b0a68ab23
yuuuuyas <yuyasakurako0110@gmail.com>
2022-01-13 11:35:51 +0900

    条件分岐追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless @item.user_id == current_user.id
+    unless @item.user_id == current_user.id && @item.order.nil?
       redirect_to root_path
     end
   end
