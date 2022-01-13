49abed291a878d39239e5f47c8b844677fec1994
yuuuuyas <yuyasakurako0110@gmail.com>
2022-01-13 15:50:31 +0900

    テストコード修正

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -73,6 +73,16 @@ RSpec.describe OrderAddress, type: :model do
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Token can't be blank")
       end
+      it 'userが紐づいていないと登録できない' do
+        @order_address.user_id = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("User can't be blank")
+      end
+      it 'itemが紐づいていないと登録できない' do
+        @order_address.item_id = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Item can't be blank")
+      end
     end
   end
 end  
